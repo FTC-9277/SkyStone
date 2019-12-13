92d89ff6ab55a1ec46d5c060a6ee400f69d92b34
kalowskywa <kalowskywa@lhprep.org>
2019-12-12 19:39:41 -0500

    yeet test

--- TeamCode/src/main/java/autos/NewFoundationMoveBlue.java
+++ TeamCode/src/main/java/autos/NewFoundationMoveBlue.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import robot.ExplosivesRobot;
 
 @Autonomous(name = "BLUE - Found. Move", group = "foundation")
-public class NewFoundationMoveBlue extends LinearOpMode {
+public class    NewFoundationMoveBlue extends LinearOpMode {
 
     ExplosivesRobot robot;
 
@@ -18,28 +18,61 @@ public class NewFoundationMoveBlue extends LinearOpMode {
 
         waitForStart();
 
-        robot.strafe(0.5, ExplosivesRobot.Direction.LEFT);
+
+        robot.driveTime(-1.0, 1500);
         wait(1500);
+
         robot.stop();
 
         wait(100);
 
+        robot.strafe(1.0, ExplosivesRobot.Direction.RIGHT);
+        wait(500);
+        robot.stop();
+        wait(500);
+
         robot.hook();
         wait(1000);
 
-        robot.turn(0.5, ExplosivesRobot.Direction.RIGHT);
-        wait(1000);
-
-        robot.drive(-0.5);
-        wait(1500);
-
+        robot.driveTime(0.75, 2000);
+        wait(500);
         robot.stop();
+
+        wait(100);
 
         robot.unhook();
         wait(1000);
 
-        robot.drive(0.5);
+        robot.strafe(1.0, ExplosivesRobot.Direction.LEFT);
+        wait(1500);
+
+        robot.stop();
+        wait(500);
+
+        robot.driveTime(-1.0,500);
+
+        robot.stop();
+        wait(650);
+
+        robot.strafe(1.0, ExplosivesRobot.Direction.RIGHT);
         wait(1000);
+
+        robot.stop();
+        wait(500);
+
+        robot.strafe(.25, ExplosivesRobot.Direction.LEFT);
+        wait(800);
+
+        robot.stop();
+        wait(500);
+
+        robot.turn(1.0, ExplosivesRobot.Direction.RIGHT);
+        wait(440);
+
+        robot.stop();
+        wait(500);
+
+        robot.driveTime(-1.0,1000);
 
         robot.stop();
 
--- TeamCode/src/main/java/autos/NewFoundationMoveRed.java
+++ TeamCode/src/main/java/autos/NewFoundationMoveRed.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import robot.ExplosivesRobot;
 
-@Autonomous(name = "RED - Found. Move", group = "foundation")
-public class NewFoundationMoveRed extends LinearOpMode {
+@Autonomous(name = "Red - Found. Move", group = "foundation")
+public class    NewFoundationMoveRed extends LinearOpMode {
 
     ExplosivesRobot robot;
 
@@ -18,27 +18,61 @@ public class NewFoundationMoveRed extends LinearOpMode {
 
         waitForStart();
 
-        robot.drive(-0.5);
-        wait(1250);
+
+        robot.driveTime(-1.0, 1500);
+        wait(1500);
 
         robot.stop();
+
+        wait(100);
+
+        robot.strafe(1.0, ExplosivesRobot.Direction.LEFT);
+        wait(500);
+        robot.stop();
+        wait(500);
 
         robot.hook();
         wait(1000);
 
-//        robot.drive(0.8);
-        robot.lDrive(-1.0);
-        robot.rDrive(-0.3);
-        wait(1700);
-
-        robot.stop();
+        robot.driveTime(0.75, 2000);
         wait(500);
+        robot.stop();
+
+        wait(100);
 
         robot.unhook();
         wait(1000);
 
-        robot.strafe(0.5, ExplosivesRobot.Direction.RIGHT);
-        wait(3000);
+        robot.strafe(1.0, ExplosivesRobot.Direction.RIGHT);
+        wait(1500);
+
+        robot.stop();
+        wait(500);
+
+        robot.driveTime(-1.0,500);
+
+        robot.stop();
+        wait(650);
+
+        robot.strafe(1.0, ExplosivesRobot.Direction.LEFT);
+        wait(1000);
+
+        robot.stop();
+        wait(500);
+
+        robot.strafe(.25, ExplosivesRobot.Direction.RIGHT);
+        wait(800);
+
+        robot.stop();
+        wait(500);
+
+        robot.turn(1.0, ExplosivesRobot.Direction.LEFT);
+        wait(440);
+
+        robot.stop();
+        wait(500);
+
+        robot.driveTime(-1.0,1000);
 
         robot.stop();
 
